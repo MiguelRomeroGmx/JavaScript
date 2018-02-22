a427b63ff184969496e78b88042055071a36587e
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-02-21 18:14:01 -0600

    Return en funciones

--- app2.js
+++ app2.js
@@ -1,1 +1,29 @@
-console.log(nombre);
+function obtenerAleatorio() {
+    return Math.random();
+}
+
+
+function obtenerNombre() {
+    return "Miguel";
+}
+
+function esMayor05() {
+    
+    if (obtenerAleatorio() > 0.5 ) {
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+
+// console.log( esMayor05() );
+
+if (esMayor05()) {
+    console.log("Es mayor que 0.5");
+}
+else{
+    console.log("Es menor que 0.5");    
+    }
