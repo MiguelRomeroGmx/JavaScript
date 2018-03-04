897cf623b6ccd6f0794e726d7fce9cc1f92ba6fc
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-03-04 17:05:42 -0600

    Objeto Numerico

---
+++ app16.js
@@ -0,0 +1,21 @@
+var a = 10.23359898665;
+
+
+console.log(a);
+console.log(a.toFixed(3));  
+
+
+
+a = a.toString();
+console.log(a);
+
+b = 17.23975397640;
+console.log(b);
+console.log(b.toPrecision(4));
+
+var c = new Number("20");
+console.log(c);
+console.log(c.valueOf());
+
+
+
