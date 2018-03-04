d338569e8227c9c510787c9b283fa12120737281
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-03-04 12:52:31 -0600

    FizzBuzz

---
+++ app8.js
@@ -0,0 +1,17 @@
+for (let index = 0; index <= 100; index++) {
+    if (index % 3 === 0) {
+        if (index % 5 === 0) {
+            console.log('FizzBuzz');
+        } else {
+            console.log('Fizz');
+        }  
+    } else if (index % 5 === 0) {
+        console.log('Buzz');
+    } else{
+        console.log(index);
+
+    }
+    
+    
+    
+}
