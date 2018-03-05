7521312024c7edf441ffb83ad1c9ecd9f93e11a3
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-03-05 17:19:48 -0600

    expresiones regulares

---
+++ app21.js
@@ -0,0 +1,18 @@
+
+
+// Expresion regular
+
+// var reg1 = RegExp("l");
+var reg2 = /a/;
+
+var texto = "Hola Mundo";
+
+var arr = texto.match(/^H/);
+var arr = texto.match(/o$/);
+
+console.log(arr);
+
+
+
+
+
