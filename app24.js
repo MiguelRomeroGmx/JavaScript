0aff751f1863ddde5bdd8fa94536a58c02b9a9de
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-03-06 12:35:16 -0600

    switch case

---
+++ app24.js
@@ -0,0 +1,24 @@
+
+
+var mes = 40;
+
+switch (mes) {
+    case (10 > 5)? 40:1 :     // si 10 myor que 5 entonces toma el valor de 40, sino toma 1
+        console.log("Enero");
+
+        break;
+    case 2:
+        console.log("Febreero");
+        break;
+    case 3:
+        console.log("Marzo");
+        break;
+
+    default:
+    console.log("Cualquier otro mes");
+    
+        break;
+}
+
+
+
